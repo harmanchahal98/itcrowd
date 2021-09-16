ee50db739e32657eafafffd7ca8118bcff1f383b
harmandeep singh <40911302+harmanchahal98@users.noreply.github.com>
2021-09-16 14:11:50 +0530

    Add files via upload

---
+++ server/controller/auth.js
@@ -0,0 +1,63 @@
+const bcrypt = require("bcrypt");
+const { connect } = require("getstream");
+const streamChat = require("stream-chat").streamChat;
+const crypto = require("crypto");
+const { StreamChat } = require("stream-chat");
+require("dotenv").config();
+
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const app_id = process.env.STREAM_APP_ID;
+
+const login = async (req, res) => {
+  try {
+    const { userName, password } = req.body;
+    const serverClient = connect(api_key, api_secret, app_id);
+    const client = StreamChat.getInstance(api_key, api_secret);
+
+    const { users } = await client.queryUsers({ name: userName });
+
+    if (!users.length)
+      return res.status(400).json({ message: "User not found" });
+
+    const success = await bcrypt.compare(password, users[0].hashedPassword);
+
+    const token = serverClient.createUserToken(users[0].id);
+
+    if(success) {
+        res.status(200).json({ token, fullName: users[0].fullName, userName, userId: users[0].id });
+    } else{
+        return res.status(500).json({ message: "Incorrect password of username" });
+    }
+  } catch (error) {
+    console.log("====================================");
+    console.log(error);
+    console.log("====================================");
+    res.status(500).json({
+      message: error,
+    });
+  }
+};
+
+const signup = async (req, res) => {
+  try {
+    const { fullName, userName, password, phoneNumber } = req.body;
+
+    const userId = crypto.randomBytes(16).toString("hex");
+    const serverClient = connect(api_key, api_secret, app_id);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const token = serverClient.createUserToken(userId);
+    res
+      .status(200)
+      .json({ token, fullName, userId, hashedPassword, phoneNumber, userName });
+  } catch (error) {
+    console.log("====================================");
+    console.log(error);
+    console.log("====================================");
+    res.status(500).json({
+      message: error,
+    });
+  }
+};
+
+module.exports = { signup, login };
